ad6b6d3b5fbcf5bb836e5e79add10f45e6ec0948
Christopher Albert <albert@alumni.tugraz.at>
2019-01-15 17:47:51 +0100

    added statistics plot

---
+++ PLOT/plot_statistics.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  8 14:51:30 2019
+
+@author: calbert
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+inpath = ['/home/calbert/mnt/draco/run/NEO-ORB/2019-01-08_alphas/',
+          '/home/calbert/mnt/draco/run/NEO-ORB/2019-01-08_alphas_sympl/',
+          '/home/calbert/mnt/draco/run/NEO-ORB/2019-01-08_alphas_sympl2/',
+          '/home/calbert/mnt/draco/run/NEO-ORB/2019-01-08_alphas_sympl3/',
+          '/home/calbert/mnt/draco/run/NEO-ORB/2019-01-08_alphas_sympl4/',
+          '/home/calbert/run/NEO-ORB/2019-01-08_alphas_sympl/']
+
+def plotdata(infile):
+    data = np.loadtxt(infile)
+    t = data[:,0]
+    fracpass = data[:,1]
+    fractrap = data[:,2]
+    
+    plt.figure(1)
+    plt.loglog(t+1e-4, fracpass+fractrap)
+    plt.xlabel('t')
+    plt.ylabel('% confined (total)')
+    plt.grid(True, which="both")
+    plt.tight_layout()
+    
+    plt.figure(2)
+    plt.loglog(t+1e-4, fractrap)
+    plt.xlabel('t')
+    plt.ylabel('% confined (trapped, relative)')
+    plt.grid(True, which="both")
+    plt.tight_layout()
+
+
+plotdata(os.path.join(inpath[0], 'confined_fraction.dat'))
+plotdata(os.path.join(inpath[1], '016/confined_fraction.dat'))
+plotdata(os.path.join(inpath[5], 'confined_fraction.dat'))
+
+plt.figure(1)
+plt.legend(['RK', '1600'])
+plt.figure(2)
+plt.legend(['RK', '1600'])
