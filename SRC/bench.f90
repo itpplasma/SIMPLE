5326ea52c3fd507ebf7064317a68019b4f6c4b68
Christopher Albert <albert@alumni.tugraz.at>
2019-08-13 12:46:15 +0200

    Benchmarks

--- SRC/bench.f90
+++ SRC/bench.f90
@@ -1,4 +1,4 @@
-program test_sympl
+module neo_orb_bench
 
 use field_can_mod, only: FieldCan_init
 use orbit_symplectic
@@ -35,92 +35,111 @@ logical :: quasi  ! use quasi-Newton (numerical Jacobian)
 logical :: tok    ! tokamak (.True.) or stellarator (.False.)
 integer :: integ_mode
 integer :: nlag
+integer :: nplagr_invar
 integer :: ncut
-character(len=128) :: infile, outfile
+character(len=*), parameter :: infile = 'wout.nc'
+character(len=*), parameter :: outfile = '/tmp/out.txt'
 
 integer, parameter :: n_tip_vars = 7
 double precision, allocatable :: var_cut(:, :)  ! r, th, ph, pph, H, Jpar
 
 double precision :: taub
 
-namelist / bench / multi, quasi, tok, integ_mode, npoiper2, nlag, ncut, infile, outfile
-
-read( *, nml = bench )
-
-if (tok) then
-    ! Initial conditions
-    z0(1) = 0.1d0  ! r
-    z0(2) = 1.5d0  ! theta
-    z0(3) = 0.0d0  ! phi
-    vpar0 = 0.0d0  ! parallel velocity
-    
-    call FieldCan_init(f, 1d-5, 1d0, vpar0, -1)
-    
-    ! Compute toroidal momentum from initial conditions
-    call eval_field(f, z0(1), z0(2), z0(3), 0)
-
-    taub = 7800d0  ! estimated bounce time
-    dt = taub/npoiper2
-else
-    call init_field(norb, infile, 5, 5, 3, 1)
-
-    rbig = rmajor*1.0d2
-    dtaumax = twopi*rbig/npoiper2
-    dtau = dtaumax
-
-    call init_params(norb, 2, 4, 3.5d6, dtau, dtaumax, 1d-8)  ! fusion alphas)
-
-    ! Initial conditions
-    z0(1) = 0.5d0    ! r
-    z0(2) = 0.0d0    ! theta
-    z0(3) = 0.314d0  ! phi
-    vpar0 = 0.0d0    ! parallel velocity
-
-
-    call eval_field(f, z0(1), z0(2), z0(3), 0)
-
-    f%mu = .5d0**2*(1.d0-vpar0**2)/f%Bmod*2d0 ! mu by factor 2 from other modules
-    f%ro0 = ro0/dsqrt(2d0) ! ro0 = mc/e*v0, different by sqrt(2) from other modules
-    f%vpar = vpar0*dsqrt(2d0) ! vpar_bar = vpar/sqrt(T/m), different by sqrt(2) from other modules
-
-    dt = twopi*rbig/npoiper2
-end if
-
-z0(4) = vpar0*f%hph + f%Aph/f%ro0  ! p_phi
-
-nt = 10000
-
-allocate(out(5,nt))
-allocate(var_cut(ncut, n_tip_vars))
-icounter = 0
-out=0d0
-out(1:4,1) = z0
-out(5,1) = f%H
-
-call orbit_sympl_init(si, f, z0, dt, 1, 1d-13, integ_mode, nlag)
-
-
-starttime = omp_get_wtime()
-call test_cuts
-endtime = omp_get_wtime()
-print *, endtime-starttime, icounter
-
-open(unit=20, file=outfile, action='write', recl=4096)
-do kt = 1, ncut
-    write(20,*) var_cut(kt, :)
-end do
-close(20)
-deallocate(var_cut)
-deallocate(out)
-
 contains
 
-subroutine test_cuts
+subroutine init_bench()
+    print *, multi, quasi, tok, integ_mode, npoiper2, nlag, nplagr_invar, &
+      ncut
+    if (tok) then
+        ! Initial conditions
+        z0(1) = 0.1d0  ! r
+        z0(2) = 1.5d0  ! theta
+        z0(3) = 0.0d0  ! phi
+        vpar0 = 0.0d0  ! parallel velocity
+        
+        call FieldCan_init(f, 1d-5, 1d0, vpar0, -1)
+        
+        ! Compute toroidal momentum from initial conditions
+        call eval_field(f, z0(1), z0(2), z0(3), 0)
+
+        taub = 7800d0  ! estimated bounce time
+    else
+        call init_field(norb, infile, 5, 5, 3, 1)
+
+        rbig = rmajor*1.0d2
+        dtaumax = twopi*rbig/npoiper2
+        dtau = dtaumax
+
+        call init_params(norb, 2, 4, 3.5d6, dtau, dtaumax, 1d-8)  ! fusion alphas)
+
+        ! Initial conditions
+        z0(1) = 0.5d0    ! r
+        z0(2) = 0.0d0    ! theta
+        z0(3) = 0.314d0  ! phi
+        vpar0 = 0.0d0    ! parallel velocity
+
+        call eval_field(f, z0(1), z0(2), z0(3), 0)
+
+        f%mu = .5d0**2*(1.d0-vpar0**2)/f%Bmod*2d0 ! mu by factor 2 from other modules
+        f%ro0 = ro0/dsqrt(2d0) ! ro0 = mc/e*v0, different by sqrt(2) from other modules
+        f%vpar = vpar0*dsqrt(2d0) ! vpar_bar = vpar/sqrt(T/m), different by sqrt(2) from other modules
+    end if
+
+    z0(4) = vpar0*f%hph + f%Aph/f%ro0  ! p_phi
+    if (.not. allocated(out)) allocate(out(5,nt))
+    if (.not. allocated(var_cut)) allocate(var_cut(ncut, n_tip_vars))
+    nt = 10000
+    out=0d0
+    out(1:4,1) = z0
+    out(5,1) = f%H
+end subroutine init_bench
+
+subroutine do_bench()
+    if (tok) then
+        dt = taub/npoiper2
+    else
+        dt = twopi*rbig/npoiper2
+    endif
+
+    if (multi) then
+        select case (integ_mode)
+            case (21)
+                call orbit_sympl_init_verlet(mi, f, z0, dt, 1, 1d-13)
+            case (22)
+                call orbit_sympl_init_mclachlan4(mi, f, z0, dt, 1, 1d-13)
+        end select
+    else
+        call orbit_sympl_init(si, f, z0, dt, 1, 1d-13, integ_mode, nlag)
+    end if
+
+    icounter = 0
+    starttime = omp_get_wtime()
+    if (ncut>0) then
+        call test_cuts(nplagr_invar)
+    else
+        call test_orbit
+    end if
+    endtime = omp_get_wtime()
+    print *, endtime-starttime, icounter
+
+    open(unit=20, file=outfile, action='write', recl=4096)
+    do kt = 1, ncut
+        write(20,*) var_cut(kt, :)
+    end do
+    close(20)
+end subroutine do_bench
+
+subroutine cleanup_bench()
+    if (allocated(var_cut)) deallocate(var_cut)
+    if (allocated(out)) deallocate(out)
+end subroutine cleanup_bench
+
+subroutine test_cuts(nplagr)
+    integer, intent(in) :: nplagr
     integer :: itip
     double precision :: vpar_old, z(4)
 
     integer, parameter :: nder = 0
-    integer, parameter :: nplagr = 4
     double precision :: orb_sten(n_tip_vars,nplagr), coef(0:nder,nplagr)
     integer :: ipoi(nplagr)
 
@@ -137,9 +156,13 @@ subroutine test_cuts
 
     do kcut=1, ncut
         do i=1, nstep_max
-
-            call orbit_timestep_sympl(si, f, ierr)
-            z = si%z
+            if (multi) then
+                call orbit_timestep_sympl_multi(mi, f, ierr)
+                z = mi%stages(1)%z
+            else
+                call orbit_timestep_sympl(si, f, ierr)
+                z = si%z
+            end if
             if (.not. ierr==0) stop 'Error'
 
             par_inv = par_inv+f%vpar**2 ! parallel adiabatic invariant
@@ -174,6 +197,9 @@ subroutine test_cuts
         end do
     end do
 end subroutine test_cuts
+
+subroutine test_orbit
+end subroutine test_orbit
 
 subroutine test_single
     do kt = 2, nt
@@ -223,4 +249,4 @@ subroutine test_multi_quasi
     end do
 end subroutine test_multi_quasi
 
-end program test_sympl
+end module neo_orb_bench
