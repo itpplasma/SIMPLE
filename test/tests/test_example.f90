1ffb8605e3b9ca88a8fec5b02fbd913527887918
dannythecore <spacecoredan@gmail.com>
2022-08-17 17:09:58 +0200

    getting closer

---
+++ test/tests/test_example.f90
@@ -0,0 +1,21 @@
+
+
+module test_example
+   use funit
+   implicit none
+   
+contains
+
+   @test
+   subroutine test_example_1()
+      integer :: i, j, k
+
+      i = 3
+      j = 2*i
+      k = 5
+      
+      @assertEqual(k, j, message = "test example 1")
+
+   end subroutine test_example_1
+   
+end module test_example
