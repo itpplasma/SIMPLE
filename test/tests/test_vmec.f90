9d4852bad5dfd0d80d529b9500733906b2b63d44
dannythecore <spacecoredan@gmail.com>
2022-09-06 18:24:37 +0200

    finally, simple and stellopt play nice. we can now test

--- test/tests/test_vmec.f90
+++ test/tests/test_vmec.f90
@@ -1,82 +1,89 @@
-program test_vmec
+module test_vmec
 use read_wout_mod, only: tosuvspace, nfp
 use util, only: pi
+use funit
 implicit none
 
+double precision :: errtol
 character(*), parameter :: filename = 'wout.nc'
-real(8) :: s, theta, varphi, errtol
-
-! For testing libstell routines
-real(8) :: gsqrt, bsupu, bsupv, jsupu, jsupv, lam
-
-! For testing own routines
-real(8) :: A_phi, A_theta, dA_phi_ds, dA_theta_ds, aiota,&
-  R, Z, alam, dR_ds, dR_dt, dR_dp, dZ_ds, dZ_dt, dZ_dp, dl_ds, dl_dt, dl_dp
-  real(8) :: Bctrvr_vartheta, Bctrvr_varphi, Bcovar_vartheta, &
-  Bcovar_varphi, sqg, Bcovar_r, Bsqrt_varphi, Bsqrt_vartheta
-
-integer, parameter :: ntheta = 100
-integer :: k
-
-call init_own
-call init_libstell
-
-errtol = 1e-13
-do s = 0d0,1d0
-    do varphi = 0d0, 2d0 * pi
-        do k = 1,ntheta
-            theta = k*2d0*pi*1.0/ntheta
-
-            ! VMEC coordinate v=nfp*varphi where nfp is the number of field periods
-            ! (e.g. nfp=5 in W7-X, built from 5 identical segments toroidally)
-            call tosuvspace(s, theta, nfp*varphi, gsqrt, bsupu, bsupv, jsupu, jsupv, lam)
-            call vmec_field(s,theta,varphi,A_theta,A_phi,dA_theta_ds,dA_phi_ds,aiota, &
-            sqg,alam,dl_ds,dl_dt,dl_dp,Bctrvr_vartheta,Bctrvr_varphi, &
-            Bcovar_r,Bcovar_vartheta,Bcovar_varphi)
-            
-            !cccc Assert outputs cccc
-            @assertequal(lam, alam, tolerance=errtol)
-
-            Bsqrt_varphi = sqrt(Bcovar_varphi * Bctrvr_varphi)
-            Bsqrt_vartheta = sqrt(Bcovar_vartheta * Bctrvr_vartheta)
-            !@assertequal()
-
-            !write(101, *) lam
-            !write(102, *) alam
-            !Bctrvr_vartheta == bsupu ausser nfp? see https://princetonuniversity.github.io/STELLOPT/VMEC.html
-            !zeta  == varphi == phi(libstell) == v/nfp
-            !no j
-            !dA_theta_ds comp. A_theta ds
-            
-        end do !k
-    end do !varphi
-end do !s
-
 
 contains
 
-  function relerr(a, b)
-    double precision :: relerr
-    double precision, intent(in) :: a, b
-    relerr = merge(0d0, (a - b)/b, b == 0d0)
-  end function relerr
-
-  subroutine init_own
-    use new_vmec_stuff_mod, only : netcdffile, multharm
-
-    netcdffile = filename
-    multharm = 7
-
-    call spline_vmec_data
-  end subroutine init_own
+    @test
+    subroutine compare_libstell()
+        real(8) :: s, theta, varphi
 
 
-  subroutine init_libstell
-    use read_wout_mod, only: read_wout_file
+        ! For testing libstell routines
+        real(8) :: gsqrt, bsupu, bsupv, jsupu, jsupv, lam
 
-    integer :: ierr
+        ! For testing own routines
+        real(8) :: A_phi, A_theta, dA_phi_ds, dA_theta_ds, aiota,&
+          R, Z, alam, dR_ds, dR_dt, dR_dp, dZ_ds, dZ_dt, dZ_dp, dl_ds, dl_dt, dl_dp
+          real(8) :: Bctrvr_vartheta, Bctrvr_varphi, Bcovar_vartheta, &
+          Bcovar_varphi, sqg, Bcovar_r, Bsqrt_varphi, Bsqrt_vartheta
 
-    call read_wout_file(filename, ierr)
-  end subroutine init_libstell
+        integer, parameter :: ntheta = 100
+        integer :: k
 
-end program test_vmec
+        call init_own
+        call init_libstell
+
+        errtol = 1e-13
+        do s = 0d0,1d0
+            do varphi = 0d0, 2d0 * pi
+                do k = 1,ntheta
+                    theta = k*2d0*pi*1.0/ntheta
+
+                    ! VMEC coordinate v=nfp*varphi where nfp is the number of field periods
+                    ! (e.g. nfp=5 in W7-X, built from 5 identical segments toroidally)
+                    call tosuvspace(s, theta, nfp*varphi, gsqrt, bsupu, bsupv, jsupu, jsupv, lam)
+                    call vmec_field(s,theta,varphi,A_theta,A_phi,dA_theta_ds,dA_phi_ds,aiota, &
+                    sqg,alam,dl_ds,dl_dt,dl_dp,Bctrvr_vartheta,Bctrvr_varphi, &
+                    Bcovar_r,Bcovar_vartheta,Bcovar_varphi)
+                    
+                    !cccc Assert outputs cccc
+                    @assertEqual(lam, alam, tolerance=errtol)
+
+                    Bsqrt_varphi = sqrt(Bcovar_varphi * Bctrvr_varphi)
+                    Bsqrt_vartheta = sqrt(Bcovar_vartheta * Bctrvr_vartheta)
+                    !@assertEqual()
+
+                    !write(101, *) lam
+                    !write(102, *) alam
+                    !Bctrvr_vartheta == bsupu ausser nfp? see https://princetonuniversity.github.io/STELLOPT/VMEC.html
+                    !zeta  == varphi == phi(libstell) == v/nfp
+                    !no j
+                    !dA_theta_ds comp. A_theta ds
+                    
+                end do !k
+            end do !varphi
+        end do !s
+
+    end subroutine compare_libstell
+
+    function relerr(a, b)
+      double precision :: relerr
+      double precision, intent(in) :: a, b
+      relerr = merge(0d0, (a - b)/b, b == 0d0)
+    end function relerr
+
+    subroutine init_own
+      use new_vmec_stuff_mod, only : netcdffile, multharm
+
+      netcdffile = filename
+      multharm = 7
+
+      call spline_vmec_data
+    end subroutine init_own
+
+
+    subroutine init_libstell
+      use read_wout_mod, only: read_wout_file
+
+      integer :: ierr
+
+      call read_wout_file(filename, ierr)
+    end subroutine init_libstell
+
+end module test_vmec
