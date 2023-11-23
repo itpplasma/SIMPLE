41554596df4d9482db75e9082846f8dae7ee5ad4
Christopher Albert <albert@alumni.tugraz.at>
2023-11-23 21:26:02 +0000

    meshgrid

--- draft/new_coordinates.py
+++ draft/new_coordinates.py
@@ -11,9 +11,9 @@ simple.init_field(tracy, "wout.nc", 3, 3, 3, 1)
 simple.init_params(tracy, 2, 4, 3.5e6, 256, 1, 1e-13)
 
 #%%
-ns = 3
-nth = 64
-nph = 1
+ns = 32
+nth = 32
+nph = 32
 
 
 s = np.linspace(0, 1, ns)
@@ -22,11 +22,14 @@ ph = np.arange(nph)*2*np.pi/nph
 
 S, TH = np.meshgrid(s, th)
 
-x = []
+x = np.zeros((ns*nth*nph, 3))
+k = 0
 for ks in np.arange(ns):
     for kth in np.arange(nth):
         for kph in np.arange(nph):
-            x.append(coord.vmec_to_cyl(s[ks], th[kth], ph[kph]))
+            x[k, :2] = coord.vmec_to_cyl(s[ks], th[kth], ph[kph])
+            x[k, 2] = ph[kph]
+            k = k+1
 x = np.array(x)
 
 plt.figure()
