40e584929ca188f101cfe167e5f038dc99a54b45
Christopher Albert <albert@alumni.tugraz.at>
2023-11-23 20:50:56 +0000

    Trying vmec to cyl

---
+++ draft/new_coordinates.py
@@ -0,0 +1,31 @@
+#%%
+from pysimple import simple, params, orbit_symplectic
+from pysimple import get_canonical_coordinates_sub as coord
+
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+tracy = params.Tracer()
+
+simple.init_field(tracy, "wout.nc", 3, 3, 3, 1)
+simple.init_params(tracy, 2, 4, 3.5e6, 256, 1, 1e-13)
+
+#%%
+ns = 3
+nth = 64
+nph = 3
+
+s = np.linspace(0, 1, ns)
+th = np.arange(nth)*2*np.pi/nth
+ph = np.arange(nph)*2*np.pi/nph
+
+x = []
+for ks in np.arange(ns):
+    for kth in np.arange(nth):
+        for kph in np.arange(nph):
+            x.append(coord.vmec_to_cyl(s[ks], th[kth], ph[kph]))
+x = np.array(x)
+
+plt.figure()
+plt.plot(x[:, 0], x[:, 1], 'o')
+# %%
