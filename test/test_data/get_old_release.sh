ba1ba46eb67befe86ad2e0f9af950e080c8c682c
dannythecore <spacecoredan@gmail.com>
2024-03-14 18:12:00 +0100

    add dir to gitignore, format hint

--- test/test_data/get_old_release.sh
+++ test/test_data/get_old_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -f
 
-read -p "Release tag to check out: " simple_release_version
+read -p "Release tag to check out [vX.Y(.Z)]: " simple_release_version
 
 git clone git@github.com:itpplasma/SIMPLE.git simple_$simple_release_version
 cd simple_$simple_release_version
