bf2466bf7e8250ea360d1836ab744298bf1e4c51
dannythecore <spacecoredan@gmail.com>
2024-03-15 15:29:07 +0100

    make everything checkoutable

--- test/test_data/get_test_data.sh
+++ test/test_data/get_test_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -f
 
-read -p "Release tag to check out [vX.Y(.Z)]: " simple_release_version
+read -p "Release tag to check out [hash, tag, or branch]: " simple_release_version
 
 wget https://github.com/hiddenSymmetries/simsopt/raw/master/tests/test_files/wout_LandremanPaul2021_QA_reactorScale_lowres_reference.nc -O wout.nc
 
 git clone git@github.com:itpplasma/SIMPLE.git simple_$simple_release_version
 cd simple_$simple_release_version
-git fetch --all --tags --prune
-git checkout tags/${simple_release_version}
+git fetch
+git checkout ${simple_release_version}
