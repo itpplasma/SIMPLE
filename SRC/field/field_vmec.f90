a127ae71a62597c72bda453eb68aa1bd390eb723
Christopher Albert <albert@tugraz.at>
2024-08-20 22:48:42 +0200

    Trying field_vmec

--- SRC/field/field_vmec.f90
+++ SRC/field/field_vmec.f90
@@ -3,44 +3,55 @@ module field_vmec
 use, intrinsic :: iso_fortran_env, only: dp => real64
 use new_vmec_stuff_mod, only: rmnc, zmns, lmns => almns, rmns, zmnc, lmnc => almnc, &
     iota => aiota, psitor => phi, s_indices => sps, xm => axm, xn => axn, s, nsurfm, &
-    n_mn => nstrm, n_s => kpar
+    n_mn => nstrm, ns => kpar, netcdffile
 use vector_potentail_mod, only: psitor_a => torflux
 
 implicit none
 
 contains
 
-subroutine init
+subroutine init_field_vmec(filename)
     use vmec_alloc_sub, only: new_allocate_vmec_stuff
     use vmecin_sub, only: vmecin
+
+    character(*), intent(in) :: filename
+
     integer :: dummy
+
+    netcdffile = filename
+    call new_allocate_vmec_stuff
     call vmecin(rmnc, zmns, lmns, rmns, zmnc, lmnc, iota, psitor, s_indices, xm, xn, &
-        s, dummy, n_mn, n_s, psitor_a)
-end subroutine init
+        s, dummy, n_mn, ns, psitor_a)
+end subroutine init_field_vmec
 
-! def vmec_to_cyl_ref(ks, th, ph):
-! R = 0.0
-! P = 0.0
-! Z = 0.0
 
-! for kmn in range(v.nstrm):
-!     m = int(v.axm[kmn])
-!     n = int(v.axn[kmn])
-!     cosphase = np.cos(m*th - n*ph)
-!     sinphase = np.sin(m*th - n*ph)
-!     R += v.rmnc[kmn,ks]*cosphase + v.rmns[kmn,ks]*sinphase
-!     P = ph
-!     Z += v.zmnc[kmn,ks]*cosphase + v.zmns[kmn,ks]*sinphase
-! return R, P, Z
+subroutine deinit_field_vmec()
+    use vmec_alloc_sub, only: new_deallocate_vmec_stuff
+    call new_deallocate_vmec_stuff
+end subroutine deinit_field_vmec
+
+
 subroutine vmec_to_cyl(ks, th, ph)
     integer, intent(in) :: ks
     real(dp), intent(in) :: th, ph
     real(dp) :: R, P, Z
 
-    ! TODO
-    R = 0.0
-    P = 0.0
-    Z = 0.0
+    real(dp) :: cosphase, sinphase
+    integer :: k_mn, m, n
+
+    R = 0d0
+    P = 0d0
+    Z = 0d0
+
+    do k_mn = 1, n_mn
+        m = int(xm(k_mn))
+        n = int(xn(k_mn))
+        cosphase = cos(m*th - n*ph)
+        sinphase = sin(m*th - n*ph)
+        R = R + rmnc(k_mn, ks)*cosphase + rmns(k_mn, ks)*sinphase
+        P = ph
+        Z = Z + zmnc(k_mn, ks)*cosphase + zmns(k_mn, ks)*sinphase
+    end do
 end subroutine vmec_to_cyl
 
 end module field_vmec
