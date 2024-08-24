26c9ed6b48127e110af5edfb6b047237713f56f8
Christopher Albert <albert@tugraz.at>
2024-08-24 22:25:24 +0200

    Testing coordinates

--- SRC/coordinates/coordinates.f90
+++ SRC/coordinates/coordinates.f90
@@ -1,11 +1,8 @@
 module simple_coordinates
 
 use, intrinsic :: iso_fortran_env, only: dp => real64
-use magfie_sub, only : TEST, CANFLUX, VMEC, BOOZER, MEISS
 
 implicit none
-
-integer, parameter :: CART = 111, CYL = 222
 
 abstract interface
     subroutine transform_i(xfrom, xto, dxto_dxfrom)
@@ -23,25 +20,43 @@ subroutine transform_vmec_to_cyl(xfrom, xto, dxto_dxfrom)
     real(dp), intent(out) :: xto(3)
     real(dp), intent(out) :: dxto_dxfrom(3,3)
 
-
+    print *, "VMEC TO CYL"
 
 end subroutine transform_vmec_to_cyl
 
 
-subroutine transform(from, to, xfrom, xto, dxto_dxfrom)
-    integer, intent(in) :: from, to
+subroutine transform_vmec_to_cart(xfrom, xto, dxto_dxfrom)
     real(dp), intent(in) :: xfrom(3)
     real(dp), intent(out) :: xto(3)
     real(dp), intent(out) :: dxto_dxfrom(3,3)
 
-    select case (from)
-    case (VMEC)
-        select case (to)
-        case (CYL)
-            call transform_vmec_to_cyl(xfrom, xto, dxto_dxfrom)
+    print *, "VMEC TO CART"
+
+end subroutine transform_vmec_to_cart
+
+
+function get_transform(from, to)
+    procedure(transform_i), pointer :: get_transform
+    character(*), intent(in) :: from, to
+
+    get_transform => null()
+
+    select case (trim(from))
+    case ('vmec')
+        select case (trim(to))
+        case ('cart')
+            get_transform => transform_vmec_to_cart
+        case ('cyl')
+            get_transform => transform_vmec_to_cyl
+        case default
+            print *, "get_transform: Unknown transform from ", from, " to ", to
+            error stop
         end select
+    case default
+        print *, "get_transform: Unknown transform from ", from
+        error stop
     end select
-end subroutine transform
+end function get_transform
 
 
 end module simple_coordinates
