1244f4a6e2701f3a6c66061003818805144fecaa
Christopher Albert <albert@alumni.tugraz.at>
2024-08-28 18:20:00 +0000

    clean: extract

--- SRC/simple_main.f90
+++ SRC/simple_main.f90
@@ -2,7 +2,6 @@ module simple_main
   use omp_lib
   use util, only: pi, twopi, sqrt2
   use velo_mod,   only : isw_field_type
-  use orbit_symplectic, only : orbit_timestep_sympl, get_val
   use simple, only : init_sympl
   use diag_mod, only : icounter
   use collis_alp, only : loacol_alpha, stost
@@ -294,16 +293,15 @@ module simple_main
   subroutine trace_orbit(anorb, ipart)
     use find_bminmax_sub, only : get_bminmax
     use magfie_sub, only : magfie
-    use alpha_lifetime_sub, only : orbit_timestep_axis
     use classification, only : trace_orbit_with_classifiers
 
     type(Tracer), intent(inout) :: anorb
     integer, intent(in) :: ipart
-    integer :: ierr_orbit, ierr_coll
+    integer :: ierr_orbit
     double precision, dimension(5) :: z
     double precision :: bmod, sqrtg
     double precision, dimension(3) :: bder, hcovar, hctrvr, hcurl
-    integer :: it, ktau
+    integer :: it
     integer(8) :: kt
     logical :: passing
 
@@ -332,26 +330,22 @@ module simple_main
     theta_vmec=z(2)
     varphi_vmec=z(3)
 
-    if(isw_field_type.eq.0) then
-        call vmec_to_can(r,theta_vmec,varphi_vmec,z(2),z(3))
-    elseif(isw_field_type.eq.2) then
-        call vmec_to_boozer(r,theta_vmec,varphi_vmec,z(2),z(3))
-    endif
+    call from_lab_coordinates(zstart(1:3, ipart), z(1:3))
 
     if (integmode>0) call init_sympl(anorb%si, anorb%f, z, dtaumin, dtaumin, relerr, integmode)
 
     call magfie(z(1:3),bmod,sqrtg,bder,hcovar,hctrvr,hcurl)
 
-  !$omp critical
+    !$omp critical
     if(num_surf > 1) then
       call get_bminmax(z(1),bmin,bmax)
     endif
     passing = z(5)**2.gt.1.d0-bmod/bmax
     trap_par(ipart) = ((1.d0-z(5)**2)*bmax/bmod-1.d0)*bmin/(bmax-bmin)
     perp_inv(ipart) = z(4)**2*(1.d0-z(5)**2)/bmod
-  !$omp end critical
+    !$omp end critical
 
-  ! Forced classification of passing as regular:
+    ! Forced classification of passing as regular:
     if(passing .and. should_skip(ipart)) then
       !$omp critical
       confpart_pass=confpart_pass+1.d0
@@ -360,65 +354,113 @@ module simple_main
     endif
 
     kt = 0
-    if (passing) then
-      !$omp atomic
-      confpart_pass(1)=confpart_pass(1)+1.d0
-    else
-      !$omp atomic
-      confpart_trap(1)=confpart_trap(1)+1.d0
-    end if
-  !
+    call increase_confined_count(1, passing)
+    !
     do it=2,ntimstep
-      do ktau=1,ntau
-        if (integmode <= 0) then
-          call orbit_timestep_axis(z, dtaumin, dtaumin, relerr, ierr_orbit)
-        else
-          if (swcoll) then  ! TODO: move this inside modules
-            anorb%si%pabs = z(4)
-            anorb%f%vpar = z(4)*z(5)*sqrt2
-            anorb%f%mu = z(4)**2*(1.d0-z(5)**2)/anorb%f%Bmod
-            anorb%si%z(4) = anorb%f%vpar*anorb%f%hph + anorb%f%Aph/anorb%f%ro0
-            call get_val(anorb%f, anorb%si%z(4)) ! for pth
-            anorb%si%pthold = anorb%f%pth
-          endif
-          call orbit_timestep_sympl(anorb%si, anorb%f, ierr_orbit)
-          z(1:3) = anorb%si%z(1:3)
-          z(4) = dsqrt(anorb%f%mu*anorb%f%Bmod+0.5d0*anorb%f%vpar**2)
-          z(5) = anorb%f%vpar/(z(4)*sqrt2)
-        endif
-
-        ! Collisions
-        if (swcoll) then
-          call stost(z, dtaumin, 1, ierr_coll)
-          if (ierr_coll /= 0) then
-            print *, 'Error in stost: ', ierr_coll, 'z = ', z, 'dtaumin = ', dtaumin
-          endif
-        endif
-
-        if(ierr_orbit.ne.0) exit
-        kt = kt+1
-      enddo
-      if(ierr_orbit.ne.0) exit
-      if(passing) then
-        !$omp atomic
-        confpart_pass(it)=confpart_pass(it)+1.d0
-      else
-        !$omp atomic
-        confpart_trap(it)=confpart_trap(it)+1.d0
-      endif
+      call macrostep(anorb, z, kt, ierr_orbit)
+      if(ierr_orbit .ne. 0) exit
+      call increase_confined_count(it, passing)
     enddo
 
     !$omp critical
-    zend(:,ipart) = z
-    if(isw_field_type.eq.0) then
-      ! TODO need to add can_to_vmec
-      ! call can_to_vmec(z(1),z(2),z(3),zend(2,ipart),zend(3,ipart))
-    elseif(isw_field_type.eq.2) then
-      call boozer_to_vmec(z(1),z(2),z(3),zend(2,ipart),zend(3,ipart))
-    endif
+    call to_lab_coordinates(z(1:3), zend(1:3,ipart))
+    zend(4:5, ipart) = z(4:5)
     times_lost(ipart) = kt*dtaumin/v0
     !$omp end critical
   end subroutine trace_orbit
+
+  subroutine macrostep(anorb, z, kt, ierr_orbit)
+    use alpha_lifetime_sub, only : orbit_timestep_axis
+    use orbit_symplectic, only : orbit_timestep_sympl
+
+    type(Tracer), intent(inout) :: anorb
+    double precision, intent(inout) :: z(5)
+    integer(8), intent(inout) :: kt
+    integer, intent(out) :: ierr_orbit
+
+    integer :: ktau
+
+    do ktau=1,ntau
+      if (integmode <= 0) then
+        call orbit_timestep_axis(z, dtaumin, dtaumin, relerr, ierr_orbit)
+      else
+        if (swcoll) call update_momentum(anorb, z)
+        call orbit_timestep_sympl(anorb%si, anorb%f, ierr_orbit)
+        call to_standard_z_coordinates(anorb, z)
+      endif
+      if (swcoll) call collide(z, dtaumin) ! Collisions
+      if (ierr_orbit .ne. 0) exit
+      kt = kt+1
+    enddo
+  end subroutine macrostep
+
+  subroutine to_standard_z_coordinates(anorb, z)
+    type(Tracer), intent(in) :: anorb
+    double precision, intent(inout) :: z(5)
+
+    z(1:3) = anorb%si%z(1:3)
+    z(4) = dsqrt(anorb%f%mu*anorb%f%Bmod+0.5d0*anorb%f%vpar**2)
+    z(5) = anorb%f%vpar/(z(4)*sqrt2)
+  end subroutine to_standard_z_coordinates
+
+  subroutine from_lab_coordinates(xlab, x)
+    double precision, intent(in) :: xlab(:)
+    double precision, intent(inout) :: x(:)
+
+    if(isw_field_type.eq.0) then
+      call vmec_to_can(xlab(1), xlab(2), xlab(3), x(2), x(3))
+    elseif(isw_field_type.eq.2) then
+      call vmec_to_boozer(xlab(1), xlab(2), xlab(3), x(2), x(3))
+    endif
+  end subroutine from_lab_coordinates
+
+  subroutine to_lab_coordinates(x, xlab)
+    double precision, intent(in) :: x(:)
+    double precision, intent(inout) :: xlab(:)
+
+    xlab = x
+    if(isw_field_type.eq.2) then
+      call boozer_to_vmec(x(1), x(2), x(3), xlab(2), xlab(3))
+    endif
+    ! TODO: add conversion to lab coordinates for other field types
+  end subroutine to_lab_coordinates
+
+  subroutine increase_confined_count(it, passing)
+    integer, intent(in) :: it
+    logical, intent(in) :: passing
+
+    if (passing) then
+      !$omp atomic
+      confpart_pass(it) = confpart_pass(it) + 1.d0
+    else
+      !$omp atomic
+      confpart_trap(it) = confpart_trap(it) + 1.d0
+    end if
+  end subroutine increase_confined_count
+
+  subroutine update_momentum(anorb, z)
+    use orbit_symplectic, only : get_val
+
+    type(Tracer), intent(inout) :: anorb
+    double precision, intent(in) :: z(5)
+
+    anorb%si%pabs = z(4)
+    anorb%f%vpar = z(4)*z(5)*sqrt2
+    anorb%f%mu = z(4)**2*(1.d0-z(5)**2)/anorb%f%Bmod
+    anorb%si%z(4) = anorb%f%vpar*anorb%f%hph + anorb%f%Aph/anorb%f%ro0
+    call get_val(anorb%f, anorb%si%z(4)) ! for pth
+    anorb%si%pthold = anorb%f%pth
+  end subroutine update_momentum
+
+  subroutine collide(z, dt)
+    double precision, intent(in) :: z(5), dt
+    integer :: ierr_coll
+
+    call stost(z, dt, 1, ierr_coll)
+    if (ierr_coll /= 0) then
+      print *, 'Error in stost: ', ierr_coll, 'z = ', z, 'dtaumin = ', dtaumin
+    endif
+  end subroutine collide
 
 
   subroutine write_output
