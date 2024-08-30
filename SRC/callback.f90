2a0d53d0a8b76c0f60852ce2ee08a6ba84a0ce6f
Christopher Albert <albert@alumni.tugraz.at>
2024-08-30 16:47:15 +0200

    callback

---
+++ SRC/callback.f90
@@ -0,0 +1,40 @@
+module callback
+
+use params, only : Tracer
+
+implicit none
+
+logical :: output_orbits_macro = .True.
+
+contains
+
+subroutine callbacks_macrostep(tracer_, ipart, itime, t, z, ierr_orbit)
+    type(Tracer), intent(inout) :: tracer_
+    integer, intent(in) :: ipart, itime
+    double precision, intent(in) :: t, z(:)
+    integer, intent(in) :: ierr_orbit
+
+    if (ierr_orbit /= 0) call write_error(tracer_, ipart, itime, t, z, ierr_orbit)
+    if (output_orbits_macro) call write_position(ipart, t, z)
+end subroutine callbacks_macrostep
+
+
+subroutine write_error(tracer_, ipart, itime, t, z, ierr_orbit)
+    type(Tracer), intent(inout) :: tracer_
+    integer, intent(in) :: ipart, itime
+    double precision, intent(in) :: t, z(:)
+    integer, intent(in) :: ierr_orbit
+
+
+    print *, 'Error ', ierr_orbit , 'orbit ', ipart, ' step ', itime, ' time ', t
+end subroutine write_error
+
+
+subroutine write_position(ipart, t, z)
+    integer, intent(in) :: ipart
+    double precision, intent(in) :: t, z(:)
+
+    write(9000+ipart, *) z
+end subroutine write_position
+
+end module callback
