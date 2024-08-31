873adaa1d646dc631a194aed59b37d8876304abf
Christopher Albert <albert@tugraz.at>
2024-08-31 11:07:40 +0200

    Run examples

--- examples/run.sh
+++ examples/run.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-../build/simple.x simple.in
+
+cmake --build ../build/simple.x -j$(nproc)
+
+pushd canflux
+../../build/simple.x
+popd
+
+pushd boozer
+../../build/simple.x
+popd
+
+pushd meiss
+../../build/simple.x
+popd
+
+pushd albert
+../../build/simple.x
+popd
