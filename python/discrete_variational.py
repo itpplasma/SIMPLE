c996e400e35409e4553c4e3477cdb165d7702c6c
alexEgger-Feiel <a.egger-feiel@student.tugraz.at>
2024-11-19 12:53:34 +0000

    Solving DVI without using root() function

--- python/discrete_variational.py
+++ python/discrete_variational.py
@@ -18,36 +18,53 @@ z[:,0] = [r0, th0, ph0]
 [H, pth, vpar] = get_val(np.array([r0,th0,ph0,pph0]))
 
 
+
 calc_fields = lambda vpar: {
     'A2': m * vpar * f.hth + qe * f.Ath / c,
-    'dA2': np.array([
-        m * vpar * f.dhth[0] + qe * f.dAth[0] / c,
-        m * vpar * f.dhth[1] + qe * f.dAth[1] / c,
-        m * vpar * f.dhth[2] + qe * f.dAth[2] / c
-    ]),
+    'dA2': m * vpar * f.dhth + qe * f.dAth / c,
+    'd2A2': m * vpar * f.d2hth + qe * f.d2Ath / c,
     'A3': m * vpar * f.hph + qe * f.Aph / c,
-    'dA3': np.array([
-        m * vpar * f.dhph[0] + qe * f.dAph[0] / c,
-        m * vpar * f.dhph[1] + qe * f.dAph[1] / c,
-        m * vpar * f.dhph[2] + qe * f.dAph[2] / c
-    ]),
+    'dA3': m * vpar * f.dhph + qe * f.dAph / c,
+    'd2A3': m * vpar * f.d2hph + qe * f.d2Aph /c,
+
     'dB': mu * f.dB,
-    'dPhie': f.dPhie
+    'd2B': mu * f.d2B,
+    'dPhie': f.dPhie,
+    'd2Phie': f.d2Phie
 }
 
-#%%
-def F(x, zold, eq2, eq3):
-    f.evaluate(x[0], x[1], zold[2])
-    cov = calc_fields(x[2])
 
-    eq1 = -(cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) / (cov['dA3'][0]) 
+def Newton(x, zold, eq2, eq3):
 
-    ret = np.zeros(3)
-    ret[0] = eq2 - (m*x[2]*f.hth + qe*f.Ath/c)
-    ret[1] = eq3 - (m*x[2]*f.hph + qe*f.Aph/c)
-    ret[2] = m*f.hth *(x[1] - zold[1]) + m*f.hph*eq1 - dt*x[2]
+    eps = 1
+    while eps >= 1e-11:
+        f.evaluate(x[0], x[1], zold[2])
+        cov = calc_fields(x[2])
 
-    return ret
+        eq1 = -(cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) / (cov['dA3'][0]) 
+        deq1 = np.array([-(cov['d2A2'][0] *(x[1]-zold[1]) - dt*(cov['d2B'][0] + cov['d2Phie'][0])) / (cov['dA3'][0]) + (cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) /(cov['dA3'][0])**2 * cov['d2A3'][0],
+                     -(cov['d2A2'][3] *(x[1]-zold[1]) + cov['dA2'][0] - dt*(cov['d2B'][3] + cov['d2Phie'][3])) / (cov['dA3'][0]) + (cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) /(cov['dA3'][0])**2 * cov['d2A3'][3],
+                     -(m*f.dhth[0] *(x[1]- zold[1]))/(cov['dA3'][0]) + (cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) /(cov['dA3'][0])**2 * (m * f.dhph[0])])
+
+        F = np.zeros(3)
+        F[0] = eq2 - cov['A2']
+        F[1] = eq3 - cov['A3']
+        F[2] = m*f.hth *(x[1] - zold[1]) + m*f.hph*eq1 - dt*x[2]
+
+        Jacobi = np.array([[-cov['dA2'][0], -cov['dA2'][1], -m*f.hth], 
+                      [-cov['dA3'][0], -cov['dA3'][1], -m*f.hph],
+                      [m*(f.dhth[0]*(x[1] - zold[1]) + f.dhph[0]*eq1 + f.hph*deq1[0]), m*(f.hth + f.dhph[1]*eq1 + f.hph*deq1[1]), m*f.hph*deq1[2] - dt]])
+
+        delta = np.linalg.solve(Jacobi, -F)
+
+        xnew = x + delta
+        x = xnew
+
+        eps = abs(F[2])
+
+
+
+    return x
 
 
 
@@ -55,16 +72,6 @@ from time import time
 tic = time()
 for kt in range(nt):
     vparold = vpar
-
-    # Initialize via Lagrange extrapolation
-    if(kt>=nlag):
-        x0 = np.zeros(2)
-        extrapr0 = lagrange(np.arange(-nlag, 1), z[0,kt-nlag:kt+1])
-        extrapr1 = lagrange(np.arange(-nlag, 1), z[1,kt-nlag:kt+1])
-        x0[0] = extrapr0(1.0)
-        x0[1] = extrapr1(1.0)
-    else:
-        x0 = np.array([z[0,kt], z[1,kt]])
 
 
     f.evaluate(z[0,kt], z[1,kt], z[2,kt])  
@@ -77,10 +84,13 @@ for kt in range(nt):
     eq2 = cov['dA2'][1]*Delta[0] + cov['dA3'][1]*Delta[1] + cov['A2'] - dt*(cov['dB'][1] + cov['dPhie'][1])
     eq3 = cov['dA2'][2]*Delta[0] + cov['dA3'][2]*Delta[1] + cov['A3'] - dt*(cov['dB'][2] + cov['dPhie'][2])
     
-    # Implicit substep for A+
-    X0 = np.append(x0 , vparold)
-    sol = root(F, X0, method='hybr',tol=1e-12,args=(z[:,kt], eq2, eq3))
-    r, th, vpar = sol.x
+    # Implicit substep with Newton
+    x0 = np.array([z[0,kt], z[1,kt], vparold])
+    x = Newton(x0, z[:,kt], eq2, eq3)
+
+    r = x[0]
+    th = x[1]
+    vpar = x[2]
     
     f.evaluate(r, th, z[2,kt+1])
     cov = calc_fields(vpar)
@@ -97,111 +107,5 @@ for kt in range(nt):
 
 plot_orbit(z)
 plt.show()
-    
-'''
 
-
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import root
-from scipy.interpolate import lagrange
-import common
-from common import f, r0, th0, ph0, pph0, timesteps, get_val, get_der
-from plotting import plot_orbit, plot_cost_function
-
-
-dt, nt = timesteps(steps_per_bounce = 8, nbounce = 100)
-nlag = 1 # order of Lagrange extrapolation
-
-qe = 1.0; m = 1.0; c = 1.0 # particle charge, mass and speed of light
-mu = 1e-5 # magnetic moment
-
-z = np.zeros([3,nt+1])
-z[:,0] = [r0, th0, ph0]
-[H, pth, vpar] = get_val(np.array([r0,th0,ph0,pph0]))
-
-
-calc_fields = lambda vpar: {
-    'A2': m * vpar * f.hth + qe * f.Ath / c,
-    'dA2': np.array([
-        m * vpar * f.dhth[0] + qe * f.dAth[0] / c,
-        m * vpar * f.dhth[1] + qe * f.dAth[1] / c,
-        m * vpar * f.dhth[2] + qe * f.dAth[2] / c
-    ]),
-    'A3': m * vpar * f.hph + qe * f.Aph / c,
-    'dA3': np.array([
-        m * vpar * f.dhph[0] + qe * f.dAph[0] / c,
-        m * vpar * f.dhph[1] + qe * f.dAph[1] / c,
-        m * vpar * f.dhph[2] + qe * f.dAph[2] / c
-    ]),
-    'dB': mu * f.dB,
-    'dPhie': f.dPhie
-}
-
-
-def F(x, zold, eq2, eq3):
-    f.evaluate(x[0], x[1], zold[2])
-    cov = calc_fields(x[2])
-
-    eq1 = -(cov['dA2'][0] *(x[1] - zold[1]) - dt *(cov['dB'][0] + cov['dPhie'][0])) / (cov['dA3'][0]) 
-
-    ret = np.zeros(3)
-    ret[0] = eq2 - (m*x[2]*f.hth + qe*f.Ath/c)
-    ret[1] = eq3 - (m*x[2]*f.hph + qe*f.Aph/c)
-    ret[2] = m*f.hth *(x[1] - zold[1]) + m*f.hph*eq1 - dt*x[2]
-
-    return ret
-
-
-
-from time import time
-tic = time()
-for kt in range(nt):
-    vparold = vpar
-
-    # Initialize via Lagrange extrapolation
-    if(kt>=nlag):
-        x0 = np.zeros(2)
-        extrapr0 = lagrange(np.arange(-nlag, 1), z[0,kt-nlag:kt+1])
-        extrapr1 = lagrange(np.arange(-nlag, 1), z[1,kt-nlag:kt+1])
-        x0[0] = extrapr0(1.0)
-        x0[1] = extrapr1(1.0)
-    else:
-        x0 = np.array([z[0,kt], z[1,kt]])
-
-
-    f.evaluate(z[0,kt], z[1,kt], z[2,kt])  
-    cov = calc_fields(vparold)
-    # M_ij * delta_j = b_i
-    M = np.array([[cov['dA2'][0], cov['dA3'][0]],[m*f.hth,  m*f.hph]])
-    b = dt * np.array([(cov['dB'][0] + cov['dPhie'][0]), (vparold)])
-    Delta = np.linalg.solve(M, b)
-
-    eq2 = cov['dA2'][1]*Delta[0] + cov['dA3'][1]*Delta[1] + cov['A2'] - dt*(cov['dB'][1] + cov['dPhie'][1])
-    eq3 = cov['dA2'][2]*Delta[0] + cov['dA3'][2]*Delta[1] + cov['A3'] - dt*(cov['dB'][2] + cov['dPhie'][2])
-    
-    # Implicit substep for A+
-    X0 = np.append(x0 , vparold)
-    sol = root(F, X0, method='hybr',tol=1e-12,args=(z[:,kt], eq2, eq3))
-    r, th, vpar = sol.x
-    
-    f.evaluate(r, th, z[2,kt+1])
-    cov = calc_fields(vpar)
-    # M_ij * delta_j = b_i
-    M = np.array([[cov['dA2'][0], cov['dA3'][0]],[m*f.hth,  m*f.hph]])
-    b = dt * np.array([(cov['dB'][0] + cov['dPhie'][0]), (vpar)])
-    Delta = np.linalg.solve(M, b)
-
-    
-    z[0,kt+1] = r
-    z[1,kt+1] = z[1,kt] + Delta[0]
-    z[2,kt+1] = 0
-
-
-
-    
-plot_orbit(z)
-plt.show()
-
-'''
 # %%
