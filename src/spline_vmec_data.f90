4637900113e18f4afdeabb2ef03346744d4cd6e0
Christopher Albert <albert@tugraz.at>
2025-07-09 10:50:10 +0200

    refactor: Split splint_vmec_data into smaller subroutines for better readability and maintainability

--- src/spline_vmec_data.f90
+++ src/spline_vmec_data.f90
@@ -326,160 +326,275 @@ contains
 !
 !ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 !
+  subroutine normalize_coordinates(s, theta, varphi, ds, is, dtheta, i_theta, dphi, i_phi)
+    use new_vmec_stuff_mod, only : n_theta, n_phi, h_theta, h_phi, nper
+    use vector_potentail_mod, only : ns, hs
+    
+    implicit none
+    
+    double precision, parameter :: twopi = 2.d0*3.14159265358979d0
+    
+    double precision, intent(in) :: s, theta, varphi
+    double precision, intent(out) :: ds, dtheta, dphi
+    integer, intent(out) :: is, i_theta, i_phi
+    
+    ds = s/hs
+    is = max(0, min(ns-1, int(ds)))
+    ds = (ds - dble(is))*hs
+    is = is + 1
+    
+    dtheta = modulo(theta, twopi)/h_theta
+    i_theta = max(0, min(n_theta-1, int(dtheta)))
+    dtheta = (dtheta - dble(i_theta))*h_theta
+    i_theta = i_theta + 1
+    
+    dphi = modulo(varphi, twopi/dble(nper))/h_phi
+    i_phi = max(0, min(n_phi-1, int(dphi)))
+    dphi = (dphi - dble(i_phi))*h_phi
+    i_phi = i_phi + 1
+    
+  end subroutine normalize_coordinates
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+  subroutine interpolate_vector_potential(s, ds, is, A_phi, A_theta, dA_phi_ds, dA_theta_ds)
+    use vector_potentail_mod, only : ns, hs, torflux, sA_phi
+    use new_vmec_stuff_mod, only : ns_A
+    
+    implicit none
+    
+    double precision, intent(in) :: s, ds
+    integer, intent(in) :: is
+    double precision, intent(out) :: A_phi, A_theta, dA_phi_ds, dA_theta_ds
+    integer :: k
+    
+    A_theta = torflux*s
+    dA_theta_ds = torflux
+    
+    if (.not. allocated(sA_phi)) call spline_vmec_data
+    A_phi = sA_phi(ns_A+1, is)
+    dA_phi_ds = 0.d0
+    
+    do k = ns_A, 1, -1
+      A_phi = sA_phi(k, is) + ds*A_phi
+      dA_phi_ds = sA_phi(k+1, is)*dble(k) + ds*dA_phi_ds
+    enddo
+    
+  end subroutine interpolate_vector_potential
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+  subroutine get_spline_coefficients_3d(is, i_theta, i_phi, stp_R, stp_Z, stp_lam, &
+                                        dstp_R_ds, dstp_Z_ds, dstp_lam_ds)
+    use new_vmec_stuff_mod, only : sR, sZ, slam, ns_s, ns_tp
+    
+    implicit none
+    
+    integer, intent(in) :: is, i_theta, i_phi
+    double precision, dimension(:,:), intent(out) :: stp_R, stp_Z, stp_lam
+    double precision, dimension(:,:), intent(out) :: dstp_R_ds, dstp_Z_ds, dstp_lam_ds
+    integer :: nstp
+    
+    nstp = ns_tp + 1
+    
+    stp_R(1:nstp, 1:nstp) = sR(ns_s+1, :, :, is, i_theta, i_phi)
+    dstp_R_ds(1:nstp, 1:nstp) = 0.d0
+    stp_Z(1:nstp, 1:nstp) = sZ(ns_s+1, :, :, is, i_theta, i_phi)
+    dstp_Z_ds(1:nstp, 1:nstp) = 0.d0
+    stp_lam(1:nstp, 1:nstp) = slam(ns_s+1, :, :, is, i_theta, i_phi)
+    dstp_lam_ds(1:nstp, 1:nstp) = 0.d0
+    
+  end subroutine get_spline_coefficients_3d
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+  subroutine interpolate_s_direction(ds, is, i_theta, i_phi, stp_R, stp_Z, stp_lam, &
+                                     dstp_R_ds, dstp_Z_ds, dstp_lam_ds)
+    use new_vmec_stuff_mod, only : sR, sZ, slam, ns_s, ns_tp
+    
+    implicit none
+    
+    double precision, intent(in) :: ds
+    integer, intent(in) :: is, i_theta, i_phi
+    double precision, dimension(:,:), intent(inout) :: stp_R, stp_Z, stp_lam
+    double precision, dimension(:,:), intent(inout) :: dstp_R_ds, dstp_Z_ds, dstp_lam_ds
+    integer :: k, nstp
+    
+    nstp = ns_tp + 1
+    
+    do k = ns_s, 1, -1
+      stp_R(1:nstp, 1:nstp) = sR(k, :, :, is, i_theta, i_phi) + ds*stp_R(1:nstp, 1:nstp)
+      dstp_R_ds(1:nstp, 1:nstp) = sR(k+1, :, :, is, i_theta, i_phi)*dble(k) + ds*dstp_R_ds(1:nstp, 1:nstp)
+      stp_Z(1:nstp, 1:nstp) = sZ(k, :, :, is, i_theta, i_phi) + ds*stp_Z(1:nstp, 1:nstp)
+      dstp_Z_ds(1:nstp, 1:nstp) = sZ(k+1, :, :, is, i_theta, i_phi)*dble(k) + ds*dstp_Z_ds(1:nstp, 1:nstp)
+      stp_lam(1:nstp, 1:nstp) = slam(k, :, :, is, i_theta, i_phi) + ds*stp_lam(1:nstp, 1:nstp)
+      dstp_lam_ds(1:nstp, 1:nstp) = slam(k+1, :, :, is, i_theta, i_phi)*dble(k) + ds*dstp_lam_ds(1:nstp, 1:nstp)
+    enddo
+    
+  end subroutine interpolate_s_direction
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+  subroutine interpolate_theta_direction(dtheta, stp_R, stp_Z, stp_lam, dstp_R_ds, dstp_Z_ds, dstp_lam_ds, &
+                                         sp_R, sp_Z, sp_lam, dsp_R_ds, dsp_Z_ds, dsp_lam_ds, &
+                                         dsp_R_dt, dsp_Z_dt, dsp_lam_dt)
+    use new_vmec_stuff_mod, only : ns_tp
+    
+    implicit none
+    
+    double precision, intent(in) :: dtheta
+    double precision, dimension(:,:), intent(in) :: stp_R, stp_Z, stp_lam
+    double precision, dimension(:,:), intent(in) :: dstp_R_ds, dstp_Z_ds, dstp_lam_ds
+    double precision, dimension(:), intent(out) :: sp_R, sp_Z, sp_lam
+    double precision, dimension(:), intent(out) :: dsp_R_ds, dsp_Z_ds, dsp_lam_ds
+    double precision, dimension(:), intent(out) :: dsp_R_dt, dsp_Z_dt, dsp_lam_dt
+    integer :: k, nstp
+    
+    nstp = ns_tp + 1
+    
+    sp_R(1:nstp) = stp_R(nstp, 1:nstp)
+    dsp_R_ds(1:nstp) = dstp_R_ds(nstp, 1:nstp)
+    dsp_R_dt(1:nstp) = 0.d0
+    sp_Z(1:nstp) = stp_Z(nstp, 1:nstp)
+    dsp_Z_ds(1:nstp) = dstp_Z_ds(nstp, 1:nstp)
+    dsp_Z_dt(1:nstp) = 0.d0
+    sp_lam(1:nstp) = stp_lam(nstp, 1:nstp)
+    dsp_lam_ds(1:nstp) = dstp_lam_ds(nstp, 1:nstp)
+    dsp_lam_dt(1:nstp) = 0.d0
+    
+    do k = ns_tp, 1, -1
+      sp_R(1:nstp) = stp_R(k, 1:nstp) + dtheta*sp_R(1:nstp)
+      dsp_R_ds(1:nstp) = dstp_R_ds(k, 1:nstp) + dtheta*dsp_R_ds(1:nstp)
+      dsp_R_dt(1:nstp) = stp_R(k+1, 1:nstp)*dble(k) + dtheta*dsp_R_dt(1:nstp)
+      
+      sp_Z(1:nstp) = stp_Z(k, 1:nstp) + dtheta*sp_Z(1:nstp)
+      dsp_Z_ds(1:nstp) = dstp_Z_ds(k, 1:nstp) + dtheta*dsp_Z_ds(1:nstp)
+      dsp_Z_dt(1:nstp) = stp_Z(k+1, 1:nstp)*dble(k) + dtheta*dsp_Z_dt(1:nstp)
+      
+      sp_lam(1:nstp) = stp_lam(k, 1:nstp) + dtheta*sp_lam(1:nstp)
+      dsp_lam_ds(1:nstp) = dstp_lam_ds(k, 1:nstp) + dtheta*dsp_lam_ds(1:nstp)
+      dsp_lam_dt(1:nstp) = stp_lam(k+1, 1:nstp)*dble(k) + dtheta*dsp_lam_dt(1:nstp)
+    enddo
+    
+  end subroutine interpolate_theta_direction
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+  subroutine interpolate_phi_direction(dphi, sp_R, sp_Z, sp_lam, dsp_R_ds, dsp_Z_ds, dsp_lam_ds, &
+                                       dsp_R_dt, dsp_Z_dt, dsp_lam_dt, &
+                                       R, Z, alam, dR_ds, dZ_ds, dl_ds, dR_dt, dZ_dt, dl_dt, &
+                                       dR_dp, dZ_dp, dl_dp)
+    use new_vmec_stuff_mod, only : ns_tp
+    
+    implicit none
+    
+    double precision, intent(in) :: dphi
+    double precision, dimension(:), intent(in) :: sp_R, sp_Z, sp_lam
+    double precision, dimension(:), intent(in) :: dsp_R_ds, dsp_Z_ds, dsp_lam_ds
+    double precision, dimension(:), intent(in) :: dsp_R_dt, dsp_Z_dt, dsp_lam_dt
+    double precision, intent(out) :: R, Z, alam, dR_ds, dZ_ds, dl_ds
+    double precision, intent(out) :: dR_dt, dZ_dt, dl_dt, dR_dp, dZ_dp, dl_dp
+    integer :: k, nstp
+    
+    nstp = ns_tp + 1
+    
+    R = sp_R(nstp)
+    dR_ds = dsp_R_ds(nstp)
+    dR_dt = dsp_R_dt(nstp)
+    dR_dp = 0.d0
+    Z = sp_Z(nstp)
+    dZ_ds = dsp_Z_ds(nstp)
+    dZ_dt = dsp_Z_dt(nstp)
+    dZ_dp = 0.d0
+    alam = sp_lam(nstp)
+    dl_ds = dsp_lam_ds(nstp)
+    dl_dt = dsp_lam_dt(nstp)
+    dl_dp = 0.d0
+    
+    do k = ns_tp, 1, -1
+      R = sp_R(k) + dphi*R
+      dR_ds = dsp_R_ds(k) + dphi*dR_ds
+      dR_dt = dsp_R_dt(k) + dphi*dR_dt
+      dR_dp = sp_R(k+1)*dble(k) + dphi*dR_dp
+      
+      Z = sp_Z(k) + dphi*Z
+      dZ_ds = dsp_Z_ds(k) + dphi*dZ_ds
+      dZ_dt = dsp_Z_dt(k) + dphi*dZ_dt
+      dZ_dp = sp_Z(k+1)*dble(k) + dphi*dZ_dp
+      
+      alam = sp_lam(k) + dphi*alam
+      dl_ds = dsp_lam_ds(k) + dphi*dl_ds
+      dl_dt = dsp_lam_dt(k) + dphi*dl_dt
+      dl_dp = sp_lam(k+1)*dble(k) + dphi*dl_dp
+    enddo
+    
+  end subroutine interpolate_phi_direction
+!
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
   subroutine splint_vmec_data(s,theta,varphi,A_phi,A_theta,dA_phi_ds,dA_theta_ds,aiota,       &
                               R,Z,alam,dR_ds,dR_dt,dR_dp,dZ_ds,dZ_dt,dZ_dp,dl_ds,dl_dt,dl_dp)
-!
-  use new_vmec_stuff_mod,   only : n_theta,n_phi,h_theta,h_phi,sR,sZ,slam,nper,ns_A,ns_s,ns_tp
-  use vector_potentail_mod, only : ns,hs,torflux,sA_phi
-!
-  implicit none
-!
-  double precision, parameter :: twopi=2.d0*3.14159265358979d0
-!
-  integer :: is,i_theta,i_phi,k
-  double precision :: ds,dtheta,dphi,rho_tor
-  double precision, intent(in) :: s,theta,varphi
-  double precision, intent(out) :: A_phi,A_theta,dA_phi_ds,dA_theta_ds,aiota,       &
-                      R,Z,alam,dR_ds,dR_dt,dR_dp,dZ_ds,dZ_dt,dZ_dp,dl_ds,dl_dt,dl_dp
-!
-  integer, parameter :: ns_max=6
-!
-  integer :: nstp
-!
-  double precision, dimension(ns_max)        :: sp_R,sp_Z,sp_lam
-  double precision, dimension(ns_max)        :: dsp_R_ds,dsp_Z_ds,dsp_lam_ds
-  double precision, dimension(ns_max)        :: dsp_R_dt,dsp_Z_dt,dsp_lam_dt
-  double precision, dimension(ns_max,ns_max) :: stp_R,stp_Z,stp_lam
-  double precision, dimension(ns_max,ns_max) :: dstp_R_ds,dstp_Z_ds,dstp_lam_ds
-!
-  nstp=ns_tp+1
-!
-  A_theta=torflux*s
-  dA_theta_ds=torflux
-!
-  ds=s/hs
-  is=max(0,min(ns-1,int(ds)))
-  ds=(ds-dble(is))*hs
-  is=is+1
-!
-  dtheta=modulo(theta,twopi)/h_theta
-  i_theta=max(0,min(n_theta-1,int(dtheta)))
-  dtheta=(dtheta-dble(i_theta))*h_theta
-  i_theta=i_theta+1
-!
-  dphi=modulo(varphi,twopi/dble(nper))/h_phi
-  i_phi=max(0,min(n_phi-1,int(dphi)))
-  dphi=(dphi-dble(i_phi))*h_phi
-  i_phi=i_phi+1
-!
-! Begin interpolation over $s$
-!
-! Vector potential $A_\varphi$ and its derivative:
-  if (.not. allocated(sA_phi)) call spline_vmec_data
-  A_phi=sA_phi(ns_A+1,is)
-  dA_phi_ds=0.d0
-!
-  do k=ns_A,1,-1
-    A_phi=sA_phi(k,is)+ds*A_phi
-    dA_phi_ds=sA_phi(k+1,is)*dble(k)+ds*dA_phi_ds
-  enddo
-!
-! R, Z and $\lambda$ and their derivatives over $s$:
-!
-  rho_tor=sqrt(s)
-  ds=rho_tor/hs
-  is=max(0,min(ns-2,int(ds)))
-  ds=(ds-dble(is))*hs
-  is=is+1
-!
-  stp_R(1:nstp,1:nstp)=sR(ns_s+1,:,:,is,i_theta,i_phi)
-  dstp_R_ds(1:nstp,1:nstp)=0.d0
-  stp_Z(1:nstp,1:nstp)=sZ(ns_s+1,:,:,is,i_theta,i_phi)
-  dstp_Z_ds(1:nstp,1:nstp)=0.d0
-  stp_lam(1:nstp,1:nstp)=slam(ns_s+1,:,:,is,i_theta,i_phi)
-  dstp_lam_ds(1:nstp,1:nstp)=0.d0
-!
-  do k=ns_s,1,-1
-    stp_R(1:nstp,1:nstp)=sR(k,:,:,is,i_theta,i_phi)+ds*stp_R(1:nstp,1:nstp)
-    dstp_R_ds(1:nstp,1:nstp)=sR(k+1,:,:,is,i_theta,i_phi)*dble(k)+ds*dstp_R_ds(1:nstp,1:nstp)
-    stp_Z(1:nstp,1:nstp)=sZ(k,:,:,is,i_theta,i_phi)+ds*stp_Z(1:nstp,1:nstp)
-    dstp_Z_ds(1:nstp,1:nstp)=sZ(k+1,:,:,is,i_theta,i_phi)*dble(k)+ds*dstp_Z_ds(1:nstp,1:nstp)
-    stp_lam(1:nstp,1:nstp)=slam(k,:,:,is,i_theta,i_phi)+ds*stp_lam(1:nstp,1:nstp)
-    dstp_lam_ds(1:nstp,1:nstp)=slam(k+1,:,:,is,i_theta,i_phi)*dble(k)+ds*dstp_lam_ds(1:nstp,1:nstp)
-  enddo
-!
-! End interpolation over $s$
-!----------------------------
-!
-  aiota=-dA_phi_ds/dA_theta_ds
-!
-! Begin interpolation over $\theta$
-!
-  sp_R(1:nstp)=stp_R(nstp,1:nstp)
-  dsp_R_ds(1:nstp)=dstp_R_ds(nstp,1:nstp)
-  dsp_R_dt(1:nstp)=0.d0
-  sp_Z(1:nstp)=stp_Z(nstp,1:nstp)
-  dsp_Z_ds(1:nstp)=dstp_Z_ds(nstp,1:nstp)
-  dsp_Z_dt(1:nstp)=0.d0
-  sp_lam(1:nstp)=stp_lam(nstp,1:nstp)
-  dsp_lam_ds(1:nstp)=dstp_lam_ds(nstp,1:nstp)
-  dsp_lam_dt(1:nstp)=0.d0
-!
-  do k=ns_tp,1,-1
-    sp_R(1:nstp)=stp_R(k,1:nstp)+dtheta*sp_R(1:nstp)
-    dsp_R_ds(1:nstp)=dstp_R_ds(k,1:nstp)+dtheta*dsp_R_ds(1:nstp)
-    dsp_R_dt(1:nstp)=stp_R(k+1,1:nstp)*dble(k)+dtheta*dsp_R_dt(1:nstp)
-!
-    sp_Z(1:nstp)=stp_Z(k,1:nstp)+dtheta*sp_Z(1:nstp)
-    dsp_Z_ds(1:nstp)=dstp_Z_ds(k,1:nstp)+dtheta*dsp_Z_ds(1:nstp)
-    dsp_Z_dt(1:nstp)=stp_Z(k+1,1:nstp)*dble(k)+dtheta*dsp_Z_dt(1:nstp)
-!
-    sp_lam(1:nstp)=stp_lam(k,1:nstp)+dtheta*sp_lam(1:nstp)
-    dsp_lam_ds(1:nstp)=dstp_lam_ds(k,1:nstp)+dtheta*dsp_lam_ds(1:nstp)
-    dsp_lam_dt(1:nstp)=stp_lam(k+1,1:nstp)*dble(k)+dtheta*dsp_lam_dt(1:nstp)
-  enddo
-!
-! End interpolation over $\theta$
-!--------------------------------
-!
-! Begin interpolation over $\varphi$
-!
-  R=sp_R(nstp)
-  dR_ds=dsp_R_ds(nstp)
-  dR_dt=dsp_R_dt(nstp)
-  dR_dp=0.d0
-  Z=sp_Z(nstp)
-  dZ_ds=dsp_Z_ds(nstp)
-  dZ_dt=dsp_Z_dt(nstp)
-  dZ_dp=0.d0
-  alam=sp_lam(nstp)
-  dl_ds=dsp_lam_ds(nstp)
-  dl_dt=dsp_lam_dt(nstp)
-  dl_dp=0.d0
-!
-  do k=ns_tp,1,-1
-    R=sp_R(k)+dphi*R
-    dR_ds=dsp_R_ds(k)+dphi*dR_ds
-    dR_dt=dsp_R_dt(k)+dphi*dR_dt
-    dR_dp=sp_R(k+1)*dble(k)+dphi*dR_dp
-!
-    Z=sp_Z(k)+dphi*Z
-    dZ_ds=dsp_Z_ds(k)+dphi*dZ_ds
-    dZ_dt=dsp_Z_dt(k)+dphi*dZ_dt
-    dZ_dp=sp_Z(k+1)*dble(k)+dphi*dZ_dp
-!
-    alam=sp_lam(k)+dphi*alam
-    dl_ds=dsp_lam_ds(k)+dphi*dl_ds
-    dl_dt=dsp_lam_dt(k)+dphi*dl_dt
-    dl_dp=sp_lam(k+1)*dble(k)+dphi*dl_dp
-  enddo
-!
-! End interpolation over $\varphi$
-!
-  dR_ds=0.5d0*dR_ds/rho_tor
-  dZ_ds=0.5d0*dZ_ds/rho_tor
-  dl_ds=0.5d0*dl_ds/rho_tor
-!
+    use vector_potentail_mod, only : ns, hs
+    use new_vmec_stuff_mod, only : ns_tp
+    
+    implicit none
+    
+    double precision, intent(in) :: s, theta, varphi
+    double precision, intent(out) :: A_phi, A_theta, dA_phi_ds, dA_theta_ds, aiota
+    double precision, intent(out) :: R, Z, alam, dR_ds, dR_dt, dR_dp, dZ_ds, dZ_dt, dZ_dp
+    double precision, intent(out) :: dl_ds, dl_dt, dl_dp
+    
+    integer, parameter :: ns_max = 6
+    integer :: is, i_theta, i_phi, is_rho
+    double precision :: ds, dtheta, dphi, ds_rho, rho_tor
+    double precision, dimension(ns_max) :: sp_R, sp_Z, sp_lam
+    double precision, dimension(ns_max) :: dsp_R_ds, dsp_Z_ds, dsp_lam_ds
+    double precision, dimension(ns_max) :: dsp_R_dt, dsp_Z_dt, dsp_lam_dt
+    double precision, dimension(ns_max, ns_max) :: stp_R, stp_Z, stp_lam
+    double precision, dimension(ns_max, ns_max) :: dstp_R_ds, dstp_Z_ds, dstp_lam_ds
+    
+    ! Normalize coordinates and compute indices
+    call normalize_coordinates(s, theta, varphi, ds, is, dtheta, i_theta, dphi, i_phi)
+    
+    ! Interpolate vector potential
+    call interpolate_vector_potential(s, ds, is, A_phi, A_theta, dA_phi_ds, dA_theta_ds)
+    
+    ! Compute rotational transform
+    aiota = -dA_phi_ds/dA_theta_ds
+    
+    ! Convert to rho coordinate for R, Z, lambda interpolation
+    rho_tor = sqrt(s)
+    ds_rho = rho_tor/hs
+    is_rho = max(0, min(ns-2, int(ds_rho)))
+    ds_rho = (ds_rho - dble(is_rho))*hs
+    is_rho = is_rho + 1
+    
+    ! Get 3D spline coefficients
+    call get_spline_coefficients_3d(is_rho, i_theta, i_phi, stp_R, stp_Z, stp_lam, &
+                                    dstp_R_ds, dstp_Z_ds, dstp_lam_ds)
+    
+    ! Interpolate in s direction
+    call interpolate_s_direction(ds_rho, is_rho, i_theta, i_phi, stp_R, stp_Z, stp_lam, &
+                                 dstp_R_ds, dstp_Z_ds, dstp_lam_ds)
+    
+    ! Interpolate in theta direction
+    call interpolate_theta_direction(dtheta, stp_R, stp_Z, stp_lam, &
+                                     dstp_R_ds, dstp_Z_ds, dstp_lam_ds, &
+                                     sp_R, sp_Z, sp_lam, dsp_R_ds, dsp_Z_ds, dsp_lam_ds, &
+                                     dsp_R_dt, dsp_Z_dt, dsp_lam_dt)
+    
+    ! Interpolate in phi direction
+    call interpolate_phi_direction(dphi, sp_R, sp_Z, sp_lam, &
+                                   dsp_R_ds, dsp_Z_ds, dsp_lam_ds, &
+                                   dsp_R_dt, dsp_Z_dt, dsp_lam_dt, &
+                                   R, Z, alam, dR_ds, dZ_ds, dl_ds, &
+                                   dR_dt, dZ_dt, dl_dt, dR_dp, dZ_dp, dl_dp)
+    
+    ! Scale derivatives by rho_tor
+    dR_ds = 0.5d0*dR_ds/rho_tor
+    dZ_ds = 0.5d0*dZ_ds/rho_tor
+    dl_ds = 0.5d0*dl_ds/rho_tor
+    
   end subroutine splint_vmec_data
 !
 !ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
