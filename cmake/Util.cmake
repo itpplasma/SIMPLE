de927ce659315cd43215b409e9a8bb9b1d265000
Christopher Albert <albert@alumni.tugraz.at>
2025-09-30 13:14:42 +0200

    Teach CI to fetch matching libneo branch

--- cmake/Util.cmake
+++ cmake/Util.cmake
@@ -6,7 +6,19 @@ function(find_or_fetch DEPENDENCY)
         message(STATUS "Using ${DEPENDENCY} in $ENV{CODE}/${DEPENDENCY}")
     else()
         set(REPO_URL https://github.com/itpplasma/${DEPENDENCY}.git)
-        get_branch_or_main(${REPO_URL} REMOTE_BRANCH)
+        set(REMOTE_BRANCH "")
+
+        if(${DEPENDENCY} STREQUAL "libneo")
+            if(DEFINED LIBNEO_BRANCH AND NOT "${LIBNEO_BRANCH}" STREQUAL "")
+                string(STRIP "${LIBNEO_BRANCH}" REMOTE_BRANCH)
+            elseif(DEFINED ENV{LIBNEO_BRANCH} AND NOT "$ENV{LIBNEO_BRANCH}" STREQUAL "")
+                string(STRIP "$ENV{LIBNEO_BRANCH}" REMOTE_BRANCH)
+            endif()
+        endif()
+
+        if(REMOTE_BRANCH STREQUAL "")
+            get_branch_or_main(${REPO_URL} REMOTE_BRANCH)
+        endif()
         message(STATUS "Using ${DEPENDENCY} branch ${REMOTE_BRANCH} from ${REPO_URL}")
 
         FetchContent_Declare(
@@ -33,16 +45,36 @@ function(get_branch_or_main REPO_URL REMOTE_BRANCH)
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
+    set(CANDIDATE_BRANCH "${BRANCH}")
+
+    if(CANDIDATE_BRANCH STREQUAL "HEAD" OR CANDIDATE_BRANCH STREQUAL "")
+        if(DEFINED ENV{GITHUB_HEAD_REF} AND NOT "$ENV{GITHUB_HEAD_REF}" STREQUAL "")
+            set(CANDIDATE_BRANCH "$ENV{GITHUB_HEAD_REF}")
+        elseif(DEFINED ENV{GITHUB_REF_NAME} AND NOT "$ENV{GITHUB_REF_NAME}" STREQUAL "")
+            set(CANDIDATE_BRANCH "$ENV{GITHUB_REF_NAME}")
+        endif()
+    endif()
+
+    if(CANDIDATE_BRANCH STREQUAL "HEAD" OR CANDIDATE_BRANCH STREQUAL "")
+        set(CANDIDATE_BRANCH "main")
+    endif()
+
+    string(STRIP "${CANDIDATE_BRANCH}" CANDIDATE_BRANCH)
+
     execute_process(
-        COMMAND git ls-remote --heads ${REPO_URL} ${BRANCH}
+        COMMAND git ls-remote --heads ${REPO_URL} ${CANDIDATE_BRANCH}
         OUTPUT_VARIABLE BRANCH_EXISTS
         ERROR_VARIABLE GIT_ERROR
+        RESULT_VARIABLE LS_REMOTE_RESULT
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if(BRANCH_EXISTS)
-        set(${REMOTE_BRANCH} ${BRANCH} PARENT_SCOPE)
+    if(LS_REMOTE_RESULT EQUAL 0 AND BRANCH_EXISTS)
+        set(${REMOTE_BRANCH} ${CANDIDATE_BRANCH} PARENT_SCOPE)
     else()
+        if(NOT CANDIDATE_BRANCH STREQUAL "main")
+            message(WARNING "Requested branch ${CANDIDATE_BRANCH} not found in ${REPO_URL}; falling back to main")
+        endif()
         set(${REMOTE_BRANCH} "main" PARENT_SCOPE)
     endif()
 endfunction()
