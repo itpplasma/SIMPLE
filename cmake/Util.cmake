f0daa316ef2987be339878ca3b0a2e0b3f29ab4c
Christopher Albert <albert@alumni.tugraz.at>
2025-10-01 13:02:25 +0200

    Add libneo test toggle and simplify dependency fetch

--- cmake/Util.cmake
+++ cmake/Util.cmake
@@ -1,42 +1,5 @@
 include(FetchContent)
 
-function(find_or_fetch DEPENDENCY)
-    if(DEFINED ENV{CODE})
-        set(${DEPENDENCY}_SOURCE_DIR $ENV{CODE}/${DEPENDENCY})
-        message(STATUS "Using ${DEPENDENCY} in $ENV{CODE}/${DEPENDENCY}")
-    else()
-        set(REPO_URL https://github.com/itpplasma/${DEPENDENCY}.git)
-        set(REMOTE_BRANCH "")
-
-        if(${DEPENDENCY} STREQUAL "libneo")
-            if(DEFINED LIBNEO_BRANCH AND NOT "${LIBNEO_BRANCH}" STREQUAL "")
-                string(STRIP "${LIBNEO_BRANCH}" REMOTE_BRANCH)
-            elseif(DEFINED ENV{LIBNEO_BRANCH} AND NOT "$ENV{LIBNEO_BRANCH}" STREQUAL "")
-                string(STRIP "$ENV{LIBNEO_BRANCH}" REMOTE_BRANCH)
-            endif()
-        endif()
-
-        if(REMOTE_BRANCH STREQUAL "")
-            get_branch_or_main(${REPO_URL} REMOTE_BRANCH)
-        endif()
-        message(STATUS "Using ${DEPENDENCY} branch ${REMOTE_BRANCH} from ${REPO_URL}")
-
-        FetchContent_Declare(
-            ${DEPENDENCY}
-            DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-            GIT_REPOSITORY ${REPO_URL}
-            GIT_TAG ${REMOTE_BRANCH}
-        )
-        FetchContent_Populate(${DEPENDENCY})
-    endif()
-
-    add_subdirectory(${${DEPENDENCY}_SOURCE_DIR}
-        ${CMAKE_CURRENT_BINARY_DIR}/${DEPENDENCY}
-        EXCLUDE_FROM_ALL
-    )
-endfunction()
-
-
 function(get_branch_or_main REPO_URL REMOTE_BRANCH)
     execute_process(
         COMMAND git rev-parse --abbrev-ref HEAD
@@ -77,4 +40,66 @@ function(get_branch_or_main REPO_URL REMOTE_BRANCH)
         endif()
         set(${REMOTE_BRANCH} "main" PARENT_SCOPE)
     endif()
+endfunction()
+
+function(find_or_fetch DEPENDENCY)
+    string(TOUPPER "${DEPENDENCY}" _dep_upper)
+    string(TOLOWER "${DEPENDENCY}" _dep_lower)
+
+    set(_source_dir "")
+    set(_use_local FALSE)
+
+    # Allow explicit override via <DEPENDENCY>_SOURCE_DIR cache variable
+    set(_override_var "${_dep_upper}_SOURCE_DIR")
+    if(DEFINED ${_override_var})
+        set(_candidate "${${_override_var}}")
+        if(IS_DIRECTORY "${_candidate}")
+            set(_source_dir "${_candidate}")
+            set(_use_local TRUE)
+        else()
+            message(FATAL_ERROR "${_override_var}='${_candidate}' is not a directory")
+        endif()
+    endif()
+
+    if(NOT _use_local AND DEFINED ENV{CODE})
+        set(_candidate "$ENV{CODE}/${_dep_lower}")
+        if(IS_DIRECTORY "${_candidate}")
+            set(_source_dir "${_candidate}")
+            set(_use_local TRUE)
+        endif()
+    endif()
+
+    if(_use_local)
+        if("${_dep_lower}" STREQUAL "libneo")
+            set(LIBNEO_ENABLE_TESTS OFF CACHE BOOL "" FORCE)
+        endif()
+        add_subdirectory("${_source_dir}" "${CMAKE_CURRENT_BINARY_DIR}/${_dep_lower}" EXCLUDE_FROM_ALL)
+        return()
+    endif()
+
+    if("${_dep_lower}" STREQUAL "libneo")
+        set(_repo_url "https://github.com/itpplasma/libneo.git")
+        set(_branch "")
+        if(DEFINED LIBNEO_BRANCH AND NOT "${LIBNEO_BRANCH}" STREQUAL "")
+            set(_branch "${LIBNEO_BRANCH}")
+        elseif(DEFINED ENV{LIBNEO_BRANCH} AND NOT "$ENV{LIBNEO_BRANCH}" STREQUAL "")
+            set(_branch "$ENV{LIBNEO_BRANCH}")
+        endif()
+        if(_branch STREQUAL "")
+            get_branch_or_main(${_repo_url} _branch)
+        endif()
+
+        set(LIBNEO_ENABLE_TESTS OFF CACHE BOOL "" FORCE)
+
+        FetchContent_Declare(${_dep_lower}
+            GIT_REPOSITORY ${_repo_url}
+            GIT_TAG ${_branch}
+            GIT_PROGRESS TRUE
+            DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        )
+        FetchContent_MakeAvailable(${_dep_lower})
+        return()
+    endif()
+
+    message(FATAL_ERROR "find_or_fetch does not know how to fetch dependency '${DEPENDENCY}'")
 endfunction()
