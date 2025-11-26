83eb2ffb4c3e7dc732533566e2982283cb38fe7e
Christopher Albert <albert@tugraz.at>
2025-11-26 23:08:01 +0100

    Extend TEST symplectic regression to perform a timestep

--- test/tests/test_sympl_testfield.f90
+++ test/tests/test_sympl_testfield.f90
@@ -1,15 +1,18 @@
 program test_sympl_testfield
+  use, intrinsic :: iso_fortran_env, only : dp => real64
   use simple_main, only : init_field
-  use simple, only : Tracer
+  use simple, only : Tracer, init_sympl
   use params, only : isw_field_type, field_input, integmode
   use magfie_sub, only : TEST
   use field_can_mod, only : evaluate
+  use orbit_symplectic, only : orbit_timestep_sympl
 
   implicit none
 
   type(Tracer) :: norb
   character(*), parameter :: vmec_file = WOUT_FILE
   integer, parameter :: ans_s = 5, ans_tp = 5, amultharm = 5
+  integer :: ierr
 
   ! Configure symplectic GC with TEST field to ensure initialization succeeds
   isw_field_type = TEST
@@ -23,5 +26,20 @@ program test_sympl_testfield
     stop 1
   end if
 
-  print *, 'TEST field initialized for symplectic mode'
+  ! Smoke step: one symplectic timestep with test field
+  norb%relerr = 1.0e-12_dp
+  norb%dtaumin = 1.0e-3_dp
+  norb%dtau = 1.0e-3_dp
+
+  call init_sympl(norb%si, norb%f, (/0.2_dp, 1.0_dp, 0.0_dp, 1.0_dp, 0.0_dp/), &
+                  norb%dtau, norb%dtaumin, norb%relerr, 1)
+
+  ierr = 0
+  call orbit_timestep_sympl(norb%si, norb%f, ierr)
+  if (ierr /= 0) then
+    print *, 'orbit_timestep_sympl failed with ierr=', ierr
+    stop 1
+  end if
+
+  print *, 'TEST field symplectic step succeeded'
 end program test_sympl_testfield
