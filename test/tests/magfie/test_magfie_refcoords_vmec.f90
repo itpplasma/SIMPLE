6c16561fdb23e652991fc20283589b4159aa5855
Christopher Albert <albert@tugraz.at>
2025-12-16 18:25:38 +0100

    test(magfie): tighten tolerances and fix s-step mapping

--- test/tests/magfie/test_magfie_refcoords_vmec.f90
+++ test/tests/magfie/test_magfie_refcoords_vmec.f90
@@ -25,12 +25,12 @@ program test_magfie_refcoords_vmec
     integer, parameter :: n_ph = 4
 
     real(dp), parameter :: hs = 1.0e-3_dp
-    real(dp), parameter :: tol_bmod = 2.0e-3_dp
-    real(dp), parameter :: tol_sqrtg = 2.0e-3_dp
-    real(dp), parameter :: tol_bder = 1.0e-2_dp
-    real(dp), parameter :: tol_hcov = 1.0e-2_dp
-    real(dp), parameter :: tol_hctr = 1.0e-2_dp
-    real(dp), parameter :: tol_hcurl = 5.0e-2_dp
+    real(dp), parameter :: tol_bmod = 5.0e-12_dp
+    real(dp), parameter :: tol_sqrtg = 5.0e-12_dp
+    real(dp), parameter :: tol_hcov = 5.0e-12_dp
+    real(dp), parameter :: tol_hctr = 5.0e-12_dp
+    real(dp), parameter :: tol_bder = 1.0e-12_dp
+    real(dp), parameter :: tol_hcurl = 5.0e-9_dp
 
     real(dp) :: r, theta, phi, phi_period
     real(dp) :: x_r(3), x_s(3)
@@ -41,14 +41,23 @@ program test_magfie_refcoords_vmec
     real(dp) :: g_s(3, 3), ginv_s(3, 3), ginv_r(3, 3), sqrtg_s
     real(dp) :: e_cov_s(3, 3)
     real(dp) :: dh(3, 3), dBdx(3)
-    real(dp) :: J, dr, ht, hp
+    real(dp) :: J, ht, hp
     integer :: i_r, i_th, i_ph
     integer :: n_failed
+    real(dp) :: max_re_bmod, max_re_sqrtg
+    real(dp) :: max_re_bder(3), max_re_hcov(3), max_re_hctr(3), max_re_hcurl(3)
 
     n_failed = 0
     phi_period = twopi/real(nper, dp)
     ht = hs*twopi
     hp = ht/5.0_dp
+
+    max_re_bmod = 0.0_dp
+    max_re_sqrtg = 0.0_dp
+    max_re_bder = 0.0_dp
+    max_re_hcov = 0.0_dp
+    max_re_hctr = 0.0_dp
+    max_re_hcurl = 0.0_dp
 
     call init_vmec('wout.nc', 5, 5, 5, dummy)
     call create_vmec_field(field)
@@ -66,15 +75,13 @@ program test_magfie_refcoords_vmec
                 x_s = [r**2, theta, phi]
 
                 J = 2.0_dp*r
-                dr = hs/J
-
                 call vmec_cs%metric_tensor(x_s, g_s, ginv_s, sqrtg_s)
                 call vmec_cs%covariant_basis(x_s, e_cov_s)
                 call metric_inverse_scaled(J, ginv_s, ginv_r)
                 sqrtg_signed = signed_jacobian(e_cov_s)*J
                 sqrtg_fd = sqrtg_signed
 
-                call magfie_refcoords_fd(field, x_r, dr, ht, hp, phi_period, &
+                call magfie_refcoords_fd(field, x_r, hs, ht, hp, phi_period, &
                                          ginv_r, sqrtg_fd, bmod_fd, bder_fd, &
                                          hcov_fd, hctr_fd, hcurl_fd, dBdx, dh)
 
@@ -83,44 +90,55 @@ program test_magfie_refcoords_vmec
                 call transform_magfie_s_to_r(J, bmod_or, sqrtg_or, bder_or, hcov_or, &
                                              hctr_or, hcurl_or)
 
+                call update_max_scalar(bmod_fd, bmod_or, max_re_bmod)
+                call update_max_scalar(sqrtg_fd, sqrtg_or, max_re_sqrtg)
+                call update_max_vec(bder_fd, bder_or, max_re_bder)
+                call update_max_vec(hcov_fd, hcov_or, max_re_hcov)
+                call update_max_vec(hctr_fd, hctr_or, max_re_hctr)
+                call update_max_vec(hcurl_fd, hcurl_or, max_re_hcurl)
+
                 if (.not. approx_rel_or_abs(bmod_fd, bmod_or, tol_bmod, &
-                                            1.0e-12_dp)) then
+                                            1.0e-14_dp)) then
                     call report_fail('bmod', x_r, bmod_fd, bmod_or, tol_bmod, n_failed)
                 end if
                 if (.not. approx_rel_or_abs(sqrtg_fd, sqrtg_or, tol_sqrtg, &
-                                            1.0e-10_dp)) then
+                                            1.0e-12_dp)) then
                     call report_fail('sqrtg', x_r, sqrtg_fd, sqrtg_or, &
                                      tol_sqrtg, n_failed)
                 end if
-                call check_vec('bder', x_r, bder_fd, bder_or, tol_bder, 1.0e-10_dp, &
+                call check_vec('bder', x_r, bder_fd, bder_or, tol_bder, 1.0e-14_dp, &
                                n_failed)
-                call check_vec('hcov', x_r, hcov_fd, hcov_or, tol_hcov, 1.0e-10_dp, &
+                call check_vec('hcov', x_r, hcov_fd, hcov_or, tol_hcov, 1.0e-14_dp, &
                                n_failed)
-                call check_vec('hctr', x_r, hctr_fd, hctr_or, tol_hctr, 1.0e-10_dp, &
+                call check_vec('hctr', x_r, hctr_fd, hctr_or, tol_hctr, 1.0e-14_dp, &
                                n_failed)
                 call check_vec('hcurl', x_r, hcurl_fd, hcurl_or, tol_hcurl, &
-                               1.0e-10_dp, n_failed)
+                               1.0e-14_dp, n_failed)
             end do
         end do
     end do
 
     if (n_failed > 0) then
+        call print_summary(max_re_bmod, max_re_sqrtg, max_re_bder, max_re_hcov, &
+                           max_re_hctr, max_re_hcurl)
         print *, '================================'
         print *, 'FAILED: ', n_failed, ' mismatch(es) in refcoords magfie test'
         print *, '================================'
         error stop 1
     end if
 
+    call print_summary(max_re_bmod, max_re_sqrtg, max_re_bder, max_re_hcov, &
+                       max_re_hctr, max_re_hcurl)
     print *, '================================'
     print *, 'PASSED: magfie via refcoords interface matches magfie_vmec (VMEC ref)'
     print *, '================================'
 
 contains
 
-    subroutine magfie_refcoords_fd(field, x, dr, dth, dph, phi_period, ginv, sqrtg, &
+    subroutine magfie_refcoords_fd(field, x, hs, dth, dph, phi_period, ginv, sqrtg, &
                                    bmod, bder, hcov, hctr, hcurl, dBdx, dh)
         type(vmec_field_t), intent(in) :: field
-        real(dp), intent(in) :: x(3), dr, dth, dph, phi_period
+        real(dp), intent(in) :: x(3), hs, dth, dph, phi_period
         real(dp), intent(in) :: ginv(3, 3), sqrtg
         real(dp), intent(out) :: bmod, bder(3), hcov(3), hctr(3), hcurl(3)
         real(dp), intent(out) :: dBdx(3), dh(3, 3)
@@ -129,19 +147,26 @@ contains
         real(dp) :: h0(3), hpv(3), hmv(3)
         real(dp) :: x_plus(3), x_minus(3)
         real(dp) :: Acov_tmp(3)
+        real(dp) :: s0, r0, r_plus, r_minus
+        real(dp) :: db_ds, dh_ds(3)
         integer :: i
 
         call field%evaluate(x, Acov_tmp, h0, B0)
         bmod = B0
         hcov = h0
 
+        r0 = x(1)
+        s0 = r0**2
+        r_plus = sqrt(s0 + hs)
+        r_minus = sqrt(max(s0 - hs, 1.0e-16_dp))
+
         do i = 1, 3
             x_plus = x
             x_minus = x
             select case (i)
             case (1)
-                x_plus(1) = x(1) + dr
-                x_minus(1) = x(1) - dr
+                x_plus(1) = r_plus
+                x_minus(1) = r_minus
             case (2)
                 x_plus(2) = wrap_angle(x(2) + dth, twopi)
                 x_minus(2) = wrap_angle(x(2) - dth, twopi)
@@ -157,8 +182,10 @@ contains
 
             select case (i)
             case (1)
-                dBdx(i) = (Bp - Bm)/(2.0_dp*dr)
-                dh(i, :) = (hpv - hmv)/(2.0_dp*dr)
+                db_ds = (Bp - Bm)/(2.0_dp*hs)
+                dh_ds = (hpv - hmv)/(2.0_dp*hs)
+                dBdx(i) = db_ds*(2.0_dp*r0)
+                dh(i, :) = dh_ds*(2.0_dp*r0)
             case (2)
                 dBdx(i) = (Bp - Bm)/(2.0_dp*dth)
                 dh(i, :) = (hpv - hmv)/(2.0_dp*dth)
@@ -290,5 +317,43 @@ contains
         c(2) = a(3)*b(1) - a(1)*b(3)
         c(3) = a(1)*b(2) - a(2)*b(1)
     end function cross_product
+
+
+    subroutine update_max_scalar(a, b, max_rel)
+        real(dp), intent(in) :: a, b
+        real(dp), intent(inout) :: max_rel
+        real(dp) :: denom, rel
+
+        denom = max(abs(b), 1.0e-14_dp)
+        rel = abs(a - b)/denom
+        max_rel = max(max_rel, rel)
+    end subroutine update_max_scalar
+
+
+    subroutine update_max_vec(a, b, max_rel)
+        real(dp), intent(in) :: a(3), b(3)
+        real(dp), intent(inout) :: max_rel(3)
+        integer :: i
+
+        do i = 1, 3
+            call update_max_scalar(a(i), b(i), max_rel(i))
+        end do
+    end subroutine update_max_vec
+
+
+    subroutine print_summary(max_re_bmod, max_re_sqrtg, max_re_bder, max_re_hcov, &
+                             max_re_hctr, max_re_hcurl)
+        real(dp), intent(in) :: max_re_bmod, max_re_sqrtg
+        real(dp), intent(in) :: max_re_bder(3), max_re_hcov(3), max_re_hctr(3)
+        real(dp), intent(in) :: max_re_hcurl(3)
+
+        print *, 'Max relative errors vs magfie_vmec (after s->rho scaling):'
+        print '(a,1x,es12.4)', '  bmod  :', max_re_bmod
+        print '(a,1x,es12.4)', '  sqrtg :', max_re_sqrtg
+        print '(a,3(1x,es12.4))', '  bder  :', max_re_bder
+        print '(a,3(1x,es12.4))', '  hcov  :', max_re_hcov
+        print '(a,3(1x,es12.4))', '  hctr  :', max_re_hctr
+        print '(a,3(1x,es12.4))', '  hcurl :', max_re_hcurl
+    end subroutine print_summary
 
 end program test_magfie_refcoords_vmec
