895c40c5af06b9e04fd625d1bde63e3a5408f8c4
slopqueue <slopqueue@localhost>
2026-07-31 10:03:23 +0000

    Resolve #508

--- src/diag_counters.f90
+++ src/diag_counters.f90
@@ -22,7 +22,8 @@ module diag_counters
         EVT_WARNING_MAXIT_REJECT_100, EVT_WARNING_MAXIT_REJECT_1E4, &
         EVT_WARNING_MAXIT_REJECT_LARGE, EVT_WARNING_MAXIT_NONFINITE, &
         EVT_RETRY_EXHAUSTED, EVT_WARNING_AXIS_CROSSING_ACCEPT, &
-        EVT_TOROIDAL_REGULARIZATION, EVT_AXIS_FO_BRIDGE, N_EVENT
+        EVT_TOROIDAL_REGULARIZATION, EVT_AXIS_FO_BRIDGE, &
+        EVT_RECOVERY_STORM, N_EVENT
     public :: diag_counters_init, count_event, diag_counters_total, &
         diag_counters_reset, event_name
 
@@ -52,7 +53,8 @@ module diag_counters
     integer, parameter :: EVT_WARNING_AXIS_CROSSING_ACCEPT = 21
     integer, parameter :: EVT_TOROIDAL_REGULARIZATION = 22
     integer, parameter :: EVT_AXIS_FO_BRIDGE = 23
-    integer, parameter :: N_EVENT = 23
+    integer, parameter :: EVT_RECOVERY_STORM = 24
+    integer, parameter :: N_EVENT = 24
 
     ! Whole cache lines per thread column keep neighbouring threads from sharing
     ! a line. The event id indexes within a column; STRIDE >= N_EVENT.
@@ -147,6 +149,8 @@ contains
             name = 'toroidal_regularization'
         case (EVT_AXIS_FO_BRIDGE)
             name = 'axis_fo_bridge'
+        case (EVT_RECOVERY_STORM)
+            name = 'recovery_storm'
         case default
             name = 'unknown'
         end select
